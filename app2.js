059da54f183bb1be948785abc6bfd9861c803b27
wuzi <549243104@qq.com>
2017-06-27 17:29:46 +0800

    pull 6/27

--- app2.js
+++ app2.js
@@ -1,13 +1,20 @@
 // 模板引擎
 var express = require('express');
 var app = express();
-var port = 7113
+var port = 7113;
+app.locals.des = 'this is 本地变量'
 
 app.set('views','./jade');
 app.set('view engine','jade');
+app.set('trust proxy',true);
 
 app.get('/',function(req, res){
     res.render('index',{title: 'Hey', message: 'Hello World', name:'wuzi', email:'549243104@qq', user:{id: 12}})
+    // console.log('1-------',app.locals)
+    // console.log(app.mountpath)
+    // console.log('2-------',req)
+    // console.log('3-------',res)
+    // console.log('123')
 })
 
 app.listen(port,function(){
